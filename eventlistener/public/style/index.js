99647c50258d1415e4c268fc244210a5e278a799
Nick <nick_raston@hotmail.com>
2019-05-23 19:36:17 +1200

    Hosting and a few other changes

---
+++ eventlistener/public/style/index.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("locationnav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("locationnav").style.width = "0";
+}
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
